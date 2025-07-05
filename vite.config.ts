f4763f5ea222da218d47554d63cb3f52d5242239
NAVEENKUMAR S <143249794+s-naveenkumar-001@users.noreply.github.com>
2025-07-05 22:50:00 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base : "/Naveenkumars-portfolio",
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
